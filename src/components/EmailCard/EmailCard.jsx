f41ce397e635a34e800bcfdfbc264675ef09a4a1
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2023-03-29 13:21:21 -0700

    Deploy

--- src/components/EmailCard/EmailCard.jsx
+++ src/components/EmailCard/EmailCard.jsx
@@ -1,9 +1,21 @@
 export default function EmailCard({ email }) {
+    function handleChangeIsReadCheckBox(evt) {
+        const val = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
+    }
+
     return (
         <div>
             <h3>{email.name}</h3>
             <h3>{email.email}</h3>
             <h3>{email.textBox}</h3>
+            <h3>{email.createdAt}</h3>
+            <div>
+                <div>read?</div>
+                <input
+                    type="checkbox" name="isReadCheckbox"
+                    checked={email.isRead}
+                    onChange={handleChangeIsReadCheckBox} />
+            </div>
             <hr />
         </div>
     )
